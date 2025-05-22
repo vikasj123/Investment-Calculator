d749bd6fc4868d7fd5d6b2806611032d027169af
vikasj123 <vikasjadhav.pro@gmail.com>
2025-05-22 09:07:21 +0000

    initial commit

---
+++ src/app/investment-results/investment-results.component.ts
@@ -0,0 +1,15 @@
+import { Component, computed, inject } from '@angular/core';
+
+import { InvestmentService } from '../investment.service';
+
+@Component({
+  selector: 'app-investment-results',
+  standalone: false,
+  templateUrl: './investment-results.component.html',
+  styleUrl: './investment-results.component.css'
+})
+export class InvestmentResultsComponent {
+  private investmentService = inject (InvestmentService);
+
+  results = computed(() => this.investmentService.resultData());
+}
---
+++ src/app/user-input/user-input.component.ts
@@ -0,0 +1,30 @@
+import { Component, signal } from '@angular/core';
+import { InvestmentService } from '../investment.service';
+
+@Component({
+  selector: 'app-user-input',
+  standalone: false,
+  templateUrl: './user-input.component.html',
+  styleUrl: './user-input.component.css',
+})
+export class UserInputComponent {
+  enteredInitialInvestment = signal('0');
+  enteredAnnualInvestment = signal('0');
+  enteredExpectedReturn = signal('5');
+  enteredDuration = signal('10');
+
+  constructor(private investmentService: InvestmentService) {}
+
+  onSubmit() {
+    this.investmentService.calculateInvestmentResults({
+      initialInvestment: +this.enteredInitialInvestment(),
+      annualInvestment: +this.enteredAnnualInvestment(),
+      expectedReturn: +this.enteredExpectedReturn(),
+      duration: +this.enteredDuration(),
+    });
+    this.enteredAnnualInvestment.set('0');
+    this.enteredInitialInvestment.set('0');
+    this.enteredExpectedReturn.set('5');
+    this.enteredDuration.set('10');
+  }
+}
